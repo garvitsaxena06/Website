5359dff69c4e2164c45a969d3a1464d4aefdec45
Garvit Saxena <garvit.saxena@scool.in>
2020-06-24 18:35:34 +0530

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -87,13 +87,6 @@ export const pageQuery = graphql`
         node {
           frontmatter {
             title
-            cover {
-              childImageSharp {
-                fluid(maxWidth: 700, quality: 90, traceSVG: { color: "#64ffda" }) {
-                  ...GatsbyImageSharpFluid_withWebp_tracedSVG
-                }
-              }
-            }
             tech
             github
             external
